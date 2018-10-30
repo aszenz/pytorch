0d903d5ab2f8abfbc300167418795b504811e7f1
aszen <aszenz@users.noreply.github.com>
2018-10-30 14:20:55 +0530

    just testing

--- torch/legacy/nn/SpatialFullConvolution.py
+++ torch/legacy/nn/SpatialFullConvolution.py
@@ -209,6 +209,7 @@ class SpatialFullConvolution(Module):
             s += ', {}, {}'.format(self.adjW, self.adjH)
 
         s += ')'
+        print('Has bias: ', hasattr(self, 'bias')
         if not hasattr(self, 'bias') or self.bias is None:
             s += ' without bias'
         return s
